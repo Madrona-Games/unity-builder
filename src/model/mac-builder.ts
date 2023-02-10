2228d47c12977e349816ef7b3af64429e90bcec5
Andrew Kahr <22359829+AndrewKahr@users.noreply.github.com>
2023-02-10 14:05:05 -0800

    Read logs for errors in build output section

--- src/model/mac-builder.ts
+++ src/model/mac-builder.ts
@@ -1,12 +1,21 @@
-import { exec } from '@actions/exec';
-import { BuildParameters } from '.';
+import { getExecOutput } from '@actions/exec';
 
 class MacBuilder {
-  public static async run(actionFolder, workspace, buildParameters: BuildParameters, silent = false) {
-    await exec('bash', [`${actionFolder}/platforms/mac/entrypoint.sh`], {
+  public static async run(actionFolder, silent = false) {
+    const result = await getExecOutput('bash', [`${actionFolder}/platforms/mac/entrypoint.sh`], {
       silent,
-      ignoreReturnCode: true,
     });
+
+    // Check for errors in the Build Results section
+    const match = result.stdout.match(/^#\s*Build results\s*#(.*)^Size:/ms);
+
+    if (match) {
+      const buildResults = match[1];
+      const errorMatch = buildResults.match(/^Errors:\s*(\d+)$/m);
+      if (errorMatch && Number.parseInt(errorMatch[1], 10) !== 0) {
+        throw new Error(`There was an error building the project. Please read the logs for details.`);
+      }
+    }
   }
 }
 
