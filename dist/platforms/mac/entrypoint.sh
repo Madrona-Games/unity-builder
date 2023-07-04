d8e6ee644a73cc8904200cecc782970c04f53dc3
AndrewKahr <22359829+AndrewKahr@users.noreply.github.com>
2023-07-04 14:49:47 -0700

    Update entrypoint.sh

--- dist/platforms/mac/entrypoint.sh
+++ dist/platforms/mac/entrypoint.sh
@@ -4,24 +4,18 @@
 # Create directories for license activation
 #
 
-ps aux
-
 sudo mkdir /Library/Application\ Support/Unity
 sudo chmod -R 777 /Library/Application\ Support/Unity
 
 ACTIVATE_LICENSE_PATH="$ACTION_FOLDER/BlankProject"
 mkdir -p "$ACTIVATE_LICENSE_PATH"
-ps aux
 
 #
 # Run steps
 #
 source $ACTION_FOLDER/platforms/mac/steps/activate.sh
-ps aux
 source $ACTION_FOLDER/platforms/mac/steps/build.sh
-ps aux
 source $ACTION_FOLDER/platforms/mac/steps/return_license.sh
-ps aux
 
 #
 # Remove license activation directory
@@ -29,9 +23,6 @@ ps aux
 
 sudo rm -r /Library/Application\ Support/Unity
 rm -r "$ACTIVATE_LICENSE_PATH"
-
-ps aux
-
 #
 # Instructions for debugging
 #
@@ -55,21 +46,26 @@ fi;
 
 ps aux
 
-pids=$(pgrep -f /Applications/Unity)
+# Get the process IDs (PIDs) of processes containing "dotnet" and "/Applications/Unity"
+pids_dotnet=$(pgrep -f dotnet)
+pids_unity=$(pgrep -f /Applications/Unity)
+
+# Combine the PIDs from both searches
+pids_combined="$pids_dotnet $pids_unity"
 
 # Check if any matching processes were found
-if [[ -n "$pids" ]]; then
-    echo "Processes found with 'dotnet' in their command:"
-    echo "$pids"
+if [[ -n "$pids_combined" ]]; then
+    echo "Processes found with 'dotnet' or '/Applications/Unity' in their command:"
+    echo "$pids_combined"
 
     # Kill each process
-    for pid in $pids; do
+    for pid in $pids_combined; do
         kill $pid
     done
 
     echo "Processes killed successfully."
 else
-    echo "No processes found with 'dotnet' in their command."
+    echo "No processes found with 'dotnet' or '/Applications/Unity' in their command."
 fi
 
 ps aux
